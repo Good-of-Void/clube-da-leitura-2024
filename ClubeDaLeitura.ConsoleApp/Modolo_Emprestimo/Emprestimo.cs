b3b60dc6fd488a352a713636aeb20f23e35ad98b
Caio Pagliosa <caio.pinipagli@gmail.com>
2024-05-08 15:45:34 -0300

    add modolo emprestimo

---
+++ ClubeDaLeitura.ConsoleApp/Modolo_Emprestimo/Emprestimo.cs
@@ -0,0 +1,22 @@
+﻿using ClubeDaLeitura.ConsoleApp.Compartilhado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubeDaLeitura.ConsoleApp.Modolo_Emprestimo
+{
+    internal class Emprestimo : EntidadeBase
+    {
+        public override void AtualizarRegistro(EntidadeBase novoegistro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<string> Validar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
