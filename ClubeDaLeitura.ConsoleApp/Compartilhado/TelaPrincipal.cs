9026dba5ef676f543230769744dfa8b94541c475
Caio Pagliosa <caio.pinipagli@gmail.com>
2024-05-08 13:52:01 -0300

    add compartilhado

---
+++ ClubeDaLeitura.ConsoleApp/Compartilhado/TelaPrincipal.cs
@@ -0,0 +1,36 @@
+﻿
+namespace ControleMedicamentos.ConsoleApp.Compartilhado
+{   
+    internal static class TelaPrincipal
+    {
+        public static char ApresentarMenuPrincipal()
+        {
+            Console.Clear();
+
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("|       Controle de Medicamentos       |");
+            Console.WriteLine("----------------------------------------");
+
+            Console.WriteLine();
+
+            Console.WriteLine("1 - Cadastro de Pacientes");
+            Console.WriteLine("2 - Cadastro de Medicamentos");
+            Console.WriteLine("3 - Cadastro de Funcionário");
+            Console.WriteLine("4 - Cadastro de Fornecedores");
+            Console.WriteLine("5 - Cadastro de Requisições de Saída");
+            Console.WriteLine("6 - Cadastro de Requisições de Entrada");
+
+            Console.WriteLine("S - Sair");
+
+            Console.WriteLine();
+
+            Console.Write("Escolha uma das opções: ");
+
+            char opcaoEscolhida = Console.ReadLine()[0];
+
+            return opcaoEscolhida;
+        }
+    }
+
+}
+
